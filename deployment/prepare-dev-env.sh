1d8370a5dbd585c236f97305a617255a17bfe7a8
Stanko Culaja <culaja@gmail.com>
2020-12-14 23:26:10 +0100

    Refactored MachineJobProcessor so domain logic is placed in separate folder and supportive layer is on the top.

--- deployment/prepare-dev-env.sh
+++ deployment/prepare-dev-env.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo $(docker-compose -f docker-compose-infrastructure.yml down)
 echo $(docker-compose -f docker-compose-infrastructure.yml up -d)
 sleep 2
 echo $($(dirname $0)/projections/create-all-projections.sh)
