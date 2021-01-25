286fa41bb3b79228e0d4eb743949f059e248f434
culaja@gmail.com <culaja@gmail.com>
2021-01-25 06:24:46 +0100

    Fixed create projection bash script.

--- deployment/projections/create-all-projections.sh
+++ deployment/projections/create-all-projections.sh
@@ -1,9 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-ES_URL="http://localhost:2113"
-ES_CRED="admin:changeit"
+ES_URL=https://node1.eventstore:2113
+ES_CRED=admin:changeit
 
-MACHINE_JOB_PROCESSOR_VIEW_PROJECTION_NAME="MachineJobProcessorView"
-MACHINE_JOB_PROCESSOR_VIEW_PROJECTION_PROJECTION_PATH="@../CommandSide/Processors/MachineJobProcessor/Domain/MachineJobProcessorViewProjection.js"
-
-echo $($(dirname $0)/create-projection.sh $ES_URL $ES_CRED $MACHINE_JOB_PROCESSOR_VIEW_PROJECTION_NAME $MACHINE_JOB_PROCESSOR_VIEW_PROJECTION_PROJECTION_PATH)
+bash ./create-projection.sh $ES_URL $ES_CRED MachineJobProcessorView "@../../CommandSide/Processors/MachineJobProcessor/Domain/MachineJobProcessorViewProjection.js"
--- deployment/projections/create-projection.sh
+++ deployment/projections/create-projection.sh
@@ -1,24 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 EVENT_STORE_URL=$1
 EVENT_STORE_CREDENTIALS=$2
 PROJECTION_NAME=$3
 PROJECTION_PATH=$4
 
-URL=$EVENT_STORE_URL/projections/continuous?name=$PROJECTION_NAME%26type=js%26enabled=true%26emit=true%26trackemittedstreams=true
+RESULT=$(curl -k -i --data-binary  ${PROJECTION_PATH} "${EVENT_STORE_URL}/projections/continuous?name=${PROJECTION_NAME}&type=js&enabled=true&emit=true&trackemittedstreams=true" -u "${EVENT_STORE_CREDENTIALS}")
 
-STATUS=$(curl -i --data-binary $PROJECTION_PATH $URL -u $EVENT_STORE_CREDENTIALS)
-
-if [[ "${STATUS}" == *"HTTP/1.1 201"* ]]; then
+if [[ ${RESULT} == *"HTTP/2 201"* ]]; then
   echo "[SUCCESS] Projection created."
   exit 0
 fi
 
-if [[ "${STATUS}" == *"HTTP/1.1 409"* ]]; then
+if [[ ${RESULT} == *"HTTP/2 409"* ]]; then
   echo "[SUCCESS] Projection already created."
   exit 0
 fi
 
 >&2 echo "error" "[ERROR] Projection was not created."
-echo "${STATUS}"
+echo "${RESULT}"
 exit 1
