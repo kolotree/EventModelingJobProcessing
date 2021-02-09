8f9aaac86acef93cd37c9a4315f8e9ba2d14e6df
Stanko Culaja <stanko.culaja@kolotree.com>
2021-02-09 14:47:29 +0100

    Added complete machine job function.

---
+++ functions/complete-machine-job/Function/Domain/CommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using JobProcessing.Abstractions;
+
+namespace Function.Domain
+{
+    internal sealed class CommandHandler : ICommandHandler<Command>
+    {
+        private readonly IStore _store;
+
+        public CommandHandler(IStore store)
+        {
+            _store = store;
+        }
+        
+        public async Task Handle(Command c)
+        {
+            var machineJob = await _store.Get<MachineJob>(c.JobStream);
+            machineJob.Execute(c);
+            await _store.SaveChanges(machineJob);
+        }
+    }
+}
---
+++ functions/complete-machine-job/Function/Domain/MachineJob.cs
@@ -0,0 +1,36 @@
+﻿using JobProcessing.Abstractions;
+using JobProcessing.Infrastructure.Serialization;
+
+namespace Function.Domain
+{
+    internal sealed class MachineJob : Stream
+    {
+        private bool _isCompleted;
+        
+        public void Execute(Command c)
+        {
+            if (!_isCompleted)
+            {
+                ApplyChange(c.ToMachineJobComplete().ToEventEnvelope());
+            }
+        }
+
+        protected override void When(EventEnvelope eventEnvelope)
+        {
+            switch (eventEnvelope.Type)
+            {
+                case nameof(NewMachineJobStarted):
+                    var newMachineJobStarted = eventEnvelope.Deserialize<NewMachineJobStarted>();
+                    SetIdentity(StreamId.AssembleFor<MachineJob>(
+                        newMachineJobStarted.FactoryId,
+                        newMachineJobStarted.MachineId,
+                        newMachineJobStarted.JobId));
+                    _isCompleted = false;
+                    break;
+                case nameof(MachineJobCompleted):
+                    _isCompleted = true;
+                    break;
+            }
+        }
+    }
+}
