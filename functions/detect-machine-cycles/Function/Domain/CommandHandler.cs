847387040d1b9a037b407122b061f9a322f497e7
Stanko Culaja <stanko.culaja@kolotree.com>
2021-02-09 15:41:38 +0100

    Added detect-machine-cycles function.

---
+++ functions/detect-machine-cycles/Function/Domain/CommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using JobProcessing.Abstractions;
+
+namespace Function.Domain
+{
+    public sealed class CommandHandler : ICommandHandler<Command>
+    {
+        private readonly IStore _store;
+
+        public CommandHandler(IStore store)
+        {
+            _store = store;
+        }
+        
+        public Task Handle(Command c) => 
+            _store.AppendTo(MachineCycles.From(c));
+    }
+}
---
+++ functions/detect-machine-cycles/Function/Domain/MachineCycles.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JobProcessing.Abstractions;
+using JobProcessing.Infrastructure.Serialization;
+
+namespace Function.Domain
+{
+    internal sealed class MachineCycles : IStream
+    {
+        private readonly IReadOnlyList<MachineCyclesDetected> _uncommittedEvents;
+
+        private MachineCycles(
+            StreamId streamId,
+            IReadOnlyList<MachineCyclesDetected> uncommittedEvents)
+        {
+            StreamId = streamId;
+            _uncommittedEvents = uncommittedEvents;
+        }
+
+        public static MachineCycles From(Command c) => new(c.MachineCyclesGlobalStream, c.ToMachineCycleDetectedList());
+
+        public StreamId StreamId { get; }
+
+        public IReadOnlyList<EventEnvelope> UncommittedEventEnvelopes => 
+            _uncommittedEvents.Select(e => e.ToEventEnvelope()).ToList();
+    }
+}
