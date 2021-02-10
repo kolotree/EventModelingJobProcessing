af0ab007a6ce45c11b82f5c2624217ae9655a88c
Stanko Culaja <stanko.culaja@kolotree.com>
2021-02-10 08:53:49 +0100

    Fixed paths.

--- deployment/projections/create-all-projections.sh
+++ deployment/projections/create-all-projections.sh
@@ -3,4 +3,4 @@
 ES_URL=https://node1.eventstore:2113
 ES_CRED=admin:changeit
 
-bash ./create-projection.sh $ES_URL $ES_CRED MachineJobProcessorView "@../../CommandSide/Processors/MachineJobProcessor/Domain/MachineJobProcessorViewProjection.js"
+bash ./create-projection.sh $ES_URL $ES_CRED MachineJobProcessorView "@../../processors/MachineJobProcessor/Domain/MachineJobProcessorViewProjection.js"
