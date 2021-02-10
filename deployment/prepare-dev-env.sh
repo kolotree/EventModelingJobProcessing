11c554adddb1cc16c1a9785f5c73e4974c4c9d6d
Stanko Culaja <stanko.culaja@kolotree.com>
2021-02-10 09:40:20 +0100

    Reordering of folders.

--- deployment/prepare-dev-env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo $(docker-compose -f docker-compose-infrastructure.yml down)
-echo $(docker-compose -f docker-compose-infrastructure.yml up -d)
-sleep 2
-echo $($(dirname $0)/projections/create-all-projections.sh)
