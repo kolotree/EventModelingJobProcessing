c2905fab2ac1a395a0bbcb750c4df6a10ba570c5
culaja@gmail.com <culaja@gmail.com>
2021-02-16 20:09:50 +0100

    Fixed processor.

--- deployment/services/projections/create-all-projections.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ES_URL=https://node1.eventstore:2113
-ES_CRED=admin:changeit
-
-bash ./create-projection.sh $ES_URL $ES_CRED MachineJobProcessorView "@../../processors/MachineJobProcessor/Domain/MachineJobProcessorViewProjection.js"
