3c6cbfa301993a1639ecddce450fbf65b91b6b88
Stanko Culaja <stanko.culaja@kolotree.com>
2021-03-11 08:21:49 +0100

    New version of nuget packages.

--- src/core-packages/dotnet/Abstractions/CommandMetadata.cs
+++ src/core-packages/dotnet/Abstractions/CommandMetadata.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace JobProcessing.Abstractions
 {
@@ -13,6 +14,12 @@ namespace JobProcessing.Abstractions
         {
             Id = id;
             CorrelationId = correlationId;
+        }
+
+        public static CommandMetadata GenerateNew()
+        {
+            var newGuid = Guid.NewGuid().ToString();
+            return new CommandMetadata(newGuid, newGuid);
         }
         
         protected override IEnumerable<object> GetEqualityComponents()
